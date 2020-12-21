515d7348618c5d2115bab5168046aace03191141
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-12-21 16:59:46 +0100

    init client

---
+++ V1/Agence/Models/Client.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Client.php
+ *
+ * An one entity of the client
+ *
+ * @author Jjeanniard
+ * @version 0.0.1
+ */
+namespace Agence\Models;
+
+
+class Client
+{
+    private int $client_id;
+
+    private string $client_lastname;
+
+    private string $client_firstname;
+
+    private string $client_email;
+
+    private string $client_phone;
+
+    private string $client_added;
+
+    private string $client_password;
+
+    private string $com_code;
+
+    /**
+     * @return int
+     */
+    public function getClientId(): int
+    {
+        return $this->client_id;
+    }
+
+    /**
+     * @param int $client_id
+     */
+    public function setClientId(int $client_id): void
+    {
+        $this->client_id = $client_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientLastname(): string
+    {
+        return $this->client_lastname;
+    }
+
+    /**
+     * @param string $client_lastname
+     */
+    public function setClientLastname(string $client_lastname): void
+    {
+        $this->client_lastname = $client_lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientFirstname(): string
+    {
+        return $this->client_firstname;
+    }
+
+    /**
+     * @param string $client_firstname
+     */
+    public function setClientFirstname(string $client_firstname): void
+    {
+        $this->client_firstname = $client_firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientEmail(): string
+    {
+        return $this->client_email;
+    }
+
+    /**
+     * @param string $client_email
+     */
+    public function setClientEmail(string $client_email): void
+    {
+        $this->client_email = $client_email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientPhone(): string
+    {
+        return $this->client_phone;
+    }
+
+    /**
+     * @param string $client_phone
+     */
+    public function setClientPhone(string $client_phone): void
+    {
+        $this->client_phone = $client_phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientAdded(): string
+    {
+        return $this->client_added;
+    }
+
+    /**
+     * @param string $client_added
+     */
+    public function setClientAdded(string $client_added): void
+    {
+        $this->client_added = $client_added;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientPassword(): string
+    {
+        return $this->client_password;
+    }
+
+    /**
+     * @param string $client_password
+     */
+    public function setClientPassword(string $client_password): void
+    {
+        $this->client_password = $client_password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComCode(): string
+    {
+        return $this->com_code;
+    }
+
+    /**
+     * @param string $com_code
+     */
+    public function setComCode(string $com_code): void
+    {
+        $this->com_code = $com_code;
+    }
+}
---
+++ V1/Agence/Models/Clients.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Clients.php
+ *
+ * desc exemple
+ *
+ * @author Jjeanniard
+ * @version 0.0.1
+ */
+namespace Agence\Models;
+
+use \PDO;
+
+class Clients
+{
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Db::getPdo();
+    }
+
+    public function getByAll() : array
+    {
+        $stmt = false;
+
+        $sql = 'SELECT * FROM clients';
+
+        if($this->pdo->query($sql)){
+            $stmt = $this->pdo->query($sql);
+        }
+
+        $users = $stmt->fetchAll(PDO::FETCH_CLASS, Client::class);
+
+        $stmt->closeCursor();
+
+        return $users;
+    }
+
+    public function getBy(string $colonne, string $values)
+    {
+
+    }
+}
