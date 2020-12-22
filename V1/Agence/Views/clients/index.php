a731e2462167d70bfb65b217ee86b699d51efe13
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-12-22 10:33:48 +0100

    Mise au pluriel et mise en forme

--- V1/Agence/Views/clients/index.php
+++ V1/Agence/Views/clients/index.php
@@ -1,13 +1,14 @@
 <table>
     <thead>
     <tr>
-        <th>Identifiant client</th>
-        <th>Le nom du client</th>
-        <th>Le prénom du client</th>
-        <th>L'email du client</th>
-        <th>Le numéro de téléphone du client</th>
-        <th>La date d'inscription du client</th>
-        <th>Le commercial rattaché au client</th>
+        <th>Identifiant client(s)</th>
+        <th>Le nom du client(s)</th>
+        <th>Le prénom du client(s)</th>
+        <th>L'email du client(s)</th>
+        <th>Le numéro de téléphone du client(s)</th>
+        <th>La date d'inscription du client(s)</th>
+        <th>Le commercial rattaché au client(s)</th>
+        <th></th>
     </tr>
     </thead>
     <tbody>
@@ -15,13 +16,17 @@
     <?php
     foreach ($datas as $client) {
         echo '<tr>
-                <td><a href=\'/clients/client/' . $client->getClientId() . '\'>' . $client->getClientId() . '</a></td>
+                <td>' . $client->getClientId() . '</a></td>
                 <td>' . $client->getClientLastname() . '</td>
                 <td>' . $client->getClientFirstname() . '</td>
                 <td>' . $client->getClientemail() . '</td>
                 <td>' . $client->getClientPhone() . '</td>
                 <td>' . $client->getClientAdded() . '</td>
                 <td>' . $client->getComcode() . '</td>
+                <td>
+                    <a href=\'/clients/client/' . $client->getClientId() . '\'>voir/edité</a>
+                    <a href=\'/clients/delete/' . $client->getClientId() . '\'>Supprimer</a>
+                </td>
               </tr>';
     }
     ?>
