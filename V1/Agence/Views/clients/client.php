7582b65a6323d555da5529cc44990d16884474e3
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-12-22 10:30:44 +0100

    second frenchie du tableau client

--- V1/Agence/Views/clients/client.php
+++ V1/Agence/Views/clients/client.php
@@ -3,13 +3,13 @@
     <?php
     foreach ($client as $clt) {
 
-        echo '<tr><th>Id</th><td>' . $clt->getClientId() . '</tr></td>'
-            . '<tr><th>Last name</th><td>' . $clt->getClientLastname() . '</tr></td>'
-            . '<tr><th>First name</th><td>' . $clt->getClientFirstname() . '</tr></td>'
-            . '<tr><th>Email</th><td>' . $clt->getClientEmail() . '</tr></td>'
-            . '<tr><th>Phone</th><td>' . $clt->getClientPhone() . '</tr></td>'
-            . '<tr><th>Added</th><td>' . $clt->getClientAdded() . '</tr></td>'
-            . '<tr><th>Sale</th><td>' . $clt->getComCode() . '</tr></td>';
+        echo '<tr><th>Identifiant</th><td>' . $clt->getClientId() . '</tr></td>
+            <tr><th>Le nom du client</th><td>' . $clt->getClientLastname() . '</tr></td>
+            <tr><th>Le prénom du client</th><td>' . $clt->getClientFirstname() . '</tr></td>
+            <tr><th>L\'email du client</th><td>' . $clt->getClientEmail() . '</tr></td>
+            <tr><th>Le numéro de téléphone du client</th><td>' . $clt->getClientPhone() . '</tr></td>
+            <tr><th>La date de l\'inscription du client</th><td>' . $clt->getClientAdded() . '</tr></td>
+            <tr><th>Le commercial rattaché au client</th><td>' . $clt->getComCode() . '</tr></td>';
 
     }
     ?>
--- V1/Agence/Views/clients/index.php
+++ V1/Agence/Views/clients/index.php
@@ -1,27 +1,27 @@
 <table>
     <thead>
-        <tr>
-            <th>Identifiant client</th>
-            <th>Le nom du client</th>
-            <th>Le prénom du client</th>
-            <th>L'email du client</th>
-            <th>Le numéro de téléphone du client</th>
-            <th>La date d'inscription du client</th>
-            <th>Le commercial rattaché au client</th>
-        </tr>
+    <tr>
+        <th>Identifiant client</th>
+        <th>Le nom du client</th>
+        <th>Le prénom du client</th>
+        <th>L'email du client</th>
+        <th>Le numéro de téléphone du client</th>
+        <th>La date d'inscription du client</th>
+        <th>Le commercial rattaché au client</th>
+    </tr>
     </thead>
     <tbody>
 
     <?php
-    foreach ($datas as $client){
+    foreach ($datas as $client) {
         echo '<tr>
-                <td><a href=\'/clients/client/'.$client->getClientId().'\'>'. $client->getClientId() .'</a></td>
-                <td>'. $client->getClientLastname() .'</td>
-                <td>'. $client->getClientFirstname() .'</td>
-                <td>'. $client->getClientemail().'</td>
-                <td>'. $client->getClientPhone() .'</td>
-                <td>'. $client->getClientAdded() .'</td>
-                <td>'. $client->getComcode() .'</td>
+                <td><a href=\'/clients/client/' . $client->getClientId() . '\'>' . $client->getClientId() . '</a></td>
+                <td>' . $client->getClientLastname() . '</td>
+                <td>' . $client->getClientFirstname() . '</td>
+                <td>' . $client->getClientemail() . '</td>
+                <td>' . $client->getClientPhone() . '</td>
+                <td>' . $client->getClientAdded() . '</td>
+                <td>' . $client->getComcode() . '</td>
               </tr>';
     }
     ?>
